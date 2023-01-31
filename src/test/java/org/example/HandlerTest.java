81288baaed143d6ce767b12ecd3b3a0aff6c0b85
Frohnholzer <m.frohnholzer@accenture.com>
2023-01-31 17:30:30 +0100

    added tests

--- src/test/java/org/example/HandlerTest.java
+++ src/test/java/org/example/HandlerTest.java
@@ -5,11 +5,16 @@ import io.undertow.server.BlockingHttpExchange;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.BufferedWriter;
 import java.io.OutputStream;
@@ -17,13 +22,17 @@ import java.io.OutputStreamWriter;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class HandlerTest {
+    Handler handler;
 
+    S3Client s3Client;
 
+    @BeforeAll
     public void setup(@TempDir Path tempDir) throws Exception {
         final int S3_MOCK_SERVER_PORT = 9999;
 
@@ -36,33 +45,54 @@ public class HandlerTest {
 
         String serviceEndpoint = String.format("http://localhost:%d", S3_MOCK_SERVER_PORT);
 
-        try (S3Client s3Client = S3Client.builder()
+        S3Client s3Client = S3Client.builder()
                 .endpointOverride(URI.create(serviceEndpoint))
                 .httpClient(UrlConnectionHttpClient.builder().build())
-                .build()) {
+                .build();
 
-            GetObjectRequest request = GetObjectRequest.builder()
-                    .bucket("my-bucket")
-                    .key("my-object")
-                    .build();
-
-            Path targetPath = tempDir.resolve("result");
-
-            s3Client.getObject(request, targetPath);
-
-            String objectContent = Files.readString(targetPath);
-            System.out.printf("Object content: %s%n", objectContent);
-            assertEquals("Hello world", objectContent);
-        }
-
-        s3Server.stop();
+        //s3Server.stop();
     }
 
     @Test
-    void listObjectsOfS3Bucket() {
+    void setupS3BucketForTutorial(){
+        Handler.setupS3BucketForTutorial(s3Client,"bucket08101919" );
 
+        HeadBucketRequest headBucketRequest = HeadBucketRequest.builder()
+                .bucket("bucket08101919")
+                .build();
+
+        assertEquals(true, s3Client.headBucket(headBucketRequest));
+    }
+    @Test
+    void createS3BucketUsingS3Waiter() {
+        Handler.createS3BucketUsingS3Waiter(s3Client, "bucket08101919");
+
+        HeadBucketRequest headBucketRequest = HeadBucketRequest.builder()
+                .bucket("bucket08101919")
+                .build();
+
+        assertEquals(true, s3Client.headBucket(headBucketRequest));
     }
 
+
+    /*@Test
+    static void listObjectsOfS3Bucket() {
+        //given(welche testvoraussetzung habe ich)
+        ListObjectsRequest listObjects = ListObjectsRequest
+                .builder()
+                .bucket("bucketname")
+                .build();
+
+        Handler.listObjectsOfS3Bucket();
+
+        ListObjectsResponse res = s3Client.listObjects(listObjects);
+        List<S3Object> objects = res.contents();
+        for (S3Object myValue : objects) {
+            logger.info("\n The name of the object is " + myValue.key());
+            logger.info("\n The owner is " + myValue.owner());
+
+    }
+*/
 
     static class S3HttpHandler implements HttpHandler {
 
@@ -87,3 +117,4 @@ public class HandlerTest {
 
     }
 }
+
